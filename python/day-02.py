a6be7f3c698d3315c007cabbf28d67bbf0c1d42b
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-04-10 00:48:34 +0800

    finish day 2 part 2 with python

--- python/day-02.py
+++ python/day-02.py
@@ -8,21 +8,6 @@ class GameStats:
     rounds: list[dict[str, int]]
 
 
-def parse_game(line: str):
-    matches = re.match(r"^Game (\d+): (.*)", line)
-    assert matches is not None
-    groups = matches.groups()
-    game_id = int(groups[0])
-    rounds = [
-        {
-            tokens.split(" ")[1]: int(tokens.split(" ")[0])
-            for tokens in round_str.split(", ")
-        }
-        for round_str in groups[1].split("; ")
-    ]
-    return GameStats(id=game_id, rounds=rounds)
-
-
 def part_1(input: list[str]) -> str:
     num_cubes = {"red": 12, "green": 13, "blue": 14}
     solution = 0
@@ -41,4 +26,28 @@ def part_1(input: list[str]) -> str:
 
 
 def part_2(input: list[str]) -> str:
-    return "Part 2"
+    solution = 0
+    for line in input:
+        game_stats = parse_game(line)
+        max_cubes = {"red": 0, "green": 0, "blue": 0}
+        for round in game_stats.rounds:
+            for color, n in round.items():
+                max_cubes[color] = max(max_cubes[color], n)
+        power = max_cubes["red"] * max_cubes["green"] * max_cubes["blue"]
+        solution += power
+    return str(solution)
+
+
+def parse_game(line: str):
+    matches = re.match(r"^Game (\d+): (.*)", line)
+    assert matches is not None
+    groups = matches.groups()
+    game_id = int(groups[0])
+    rounds = [
+        {
+            tokens.split(" ")[1]: int(tokens.split(" ")[0])
+            for tokens in round_str.split(", ")
+        }
+        for round_str in groups[1].split("; ")
+    ]
+    return GameStats(id=game_id, rounds=rounds)
