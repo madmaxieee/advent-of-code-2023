4151e932f40d67101a42dc121e231587dcc04d9e
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-05-01 02:58:23 +0800

    finish day 4 part 2 with python

--- python/day-04.py
+++ python/day-04.py
@@ -4,12 +4,19 @@ from dataclasses import dataclass
 
 def part_1(input: list[str]) -> str:
     cards = [ScratchCard.from_string(line) for line in input]
-    total_score = sum(card.calculate_score() for card in cards)
+    total_score = sum(2 ** card.num_matches() // 2 for card in cards)
     return str(total_score)
 
 
 def part_2(input: list[str]) -> str:
-    return "Part 2"
+    cards = [ScratchCard.from_string(line) for line in input]
+    num_copies = [1] * len(cards)
+    for card in cards:
+        score = card.num_matches()
+        for i in range(score):
+            if card.id + i < len(cards):
+                num_copies[card.id + i] += num_copies[card.id - 1]
+    return str(sum(num_copies))
 
 
 @dataclass
@@ -28,9 +35,9 @@ class ScratchCard:
         my_numbers = list(map(int, re.split(r"\s+", groups[2].strip())))
         return cls(id=card_id, winning_numbers=winning_numbers, my_numbers=my_numbers)
 
-    def calculate_score(self) -> int:
-        score = 1
+    def num_matches(self) -> int:
+        score = 0
         for n in self.my_numbers:
             if n in self.winning_numbers:
-                score *= 2
-        return score // 2
+                score += 1
+        return score
