87efa546f55a59eec0ec3f9f5421abb030aeb07e
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-11-23 02:52:54 +0800

    finish day 11 part 2 with python

--- python/day-11.py
+++ python/day-11.py
@@ -20,7 +20,33 @@ def part_1(input: list[str]):
 
 
 def part_2(input: list[str]):
-    return "Part 2"
+    image = [list(row) for row in input]
+    empty_rows = {i for i in range(len(image)) if all(c == "." for c in image[i])}
+    empty_cols = {
+        i
+        for i in range(len(image[0]))
+        if all(image[j][i] == "." for j in range(len(image)))
+    }
+    galaxy_positions = [
+        (i, j)
+        for i in range(len(image))
+        for j in range(len(image[i]))
+        if image[i][j] == "#"
+    ]
+    total_distance = 0
+    expansion_factor = 1_000_000
+    for a, b in combinations(range(len(galaxy_positions)), 2):
+        g_a = galaxy_positions[a]
+        g_b = galaxy_positions[b]
+        distance = abs(g_a[0] - g_b[0]) + abs(g_a[1] - g_b[1])
+        distance += (expansion_factor - 1) * sum(
+            (i in empty_rows) for i in range(min(g_a[0], g_b[0]), max(g_a[0], g_b[0]))
+        )
+        distance += (expansion_factor - 1) * sum(
+            (j in empty_cols) for j in range(min(g_a[1], g_b[1]), max(g_a[1], g_b[1]))
+        )
+        total_distance += distance
+    return total_distance
 
 
 def expand_image(image: list[list[str]]):
